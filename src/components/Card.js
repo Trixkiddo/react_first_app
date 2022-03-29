5629db96ad3c9b3c5b9abe27664eac01e3106865
Flavie Parmentier <flavie.parmentier@gmail.com>
2022-03-29 11:59:46 +0200

    created filter

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,19 @@ import React from 'react';
 
 const Card = ({ country }) => {
   return (
-    <li className='card'>
-      <img src={country.flags.svg} alt={"drapeau " + country.translations.fra.common} />
-      <div className="infos">
-        <h3>{country.translations.fra.common}</h3>
-        <h4>{country.capital}</h4>
-        <p>Population {country.population.toLocaleString()}</p>
-      </div>
-    </li>
+    <div className='card-container'>
+      <li className="card">
+        <img
+          src={country.flags.svg}
+          alt={"drapeau " + country.translations.fra.common}
+        />
+        <div className="infos">
+          <h3>{country.translations.fra.common}</h3>
+          <h4>{country.capital}</h4>
+          <p>Population {country.population.toLocaleString()}</p> {/* toLocaleString formate l'affichage du nombre */}
+        </div>
+      </li>
+    </div>
   );
 };
 
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,8 @@ import Card from "./Card";
 const Countries = () => {
   // [état initial, fonction de mise à jour de cet état] = retourne un array à 2 entrées
   const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36); // définition variable + de sa mise à jour = on démarre en affichant 36 pays (avant tri)
+  // dynamique, si rangeValue change, elle change aussi dans le return
 
   // On va chercher la donnée de l'API
   useEffect(() => { //useEffect se joue lorsque le composant est monté
@@ -16,12 +18,22 @@ const Countries = () => {
   return (
     <div className='countries'>
       <ul className='radio-container'>
-          {data.map((country, index) => ( // Itération sur array des 250 pays.
-            <Card key={index} country={country} /> /* une clef pour chaque pays, on passe la donnée country (à l'enfant Card) qui correspond à chaque tour de .map */
+        <input
+        type="range"
+        min="1" max="250"
+        defaultValue={rangeValue} /* barre de tri dynamique qui démarre à 36 pays par défaut */
+        onChange={(event) => setRangeValue(event.target.value)}  /* évenement qui récupère la valeur de l'input filtre */
+        />
+      </ul>
+        <ul>
+          {data
+            .slice(0, rangeValue)     // entre 0 et le nombre donné de pays dans l'input
+            .map((country, index) => ( // on itére
+              <Card key={index} country={country} /> /* une clef pour chaque pays, on passe la donnée country (à l'enfant Card) qui correspond à chaque tour de .map */
           //{} pour interpoler en React
           /* /!\ donner une clef unique à chaque élément listé dans le .map grâce à l'index */
           ))}
-      </ul>
+       </ul>
     </div>
   );
 };
